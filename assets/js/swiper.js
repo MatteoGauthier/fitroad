41d2acf66d3546fc4077451b9a942d74d5346d6a
martindochamp <martinpaulcondet@gmail.com>
2020-12-10 14:55:16 +0100

    video et json

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,223 +1,235 @@
-        /* LikeCarousel (c) 2019 Simone P.M. github.com/simonepm - Licensed MIT */
+/* JSON datas */
 
-        class Carousel {
+var request = new XMLHttpRequest();
+request.open("GET", "/assets/exercices.json", false);
+request.send(null)
+var exsData = JSON.parse(request.responseText);
 
-         constructor(element) {
+var groupMuscular = 0;
+var level = 0;
 
-             this.board = element
+/* LikeCarousel (c) 2019 Simone P.M. github.com/simonepm - Licensed MIT */
 
-             // add first two cards programmatically
-             this.push()
-             this.push()
+class Carousel {
 
-             // handle gestures
-             this.handle()
+   constructor(element) {
 
-         }
+      this.board = element
 
-         handle() {
+      // add first two cards programmatically
+      this.push()
+      this.push()
 
-             // list all cards
-             this.cards = this.board.querySelectorAll('.card')
+      // handle gestures
+      this.handle()
 
-             // get top card
-             this.topCard = this.cards[this.cards.length - 1]
+   }
 
-             // get next card
-             this.nextCard = this.cards[this.cards.length - 2]
+   handle() {
 
-             // if at least one card is present
-             if (this.cards.length > 0) {
+      // list all cards
+      this.cards = this.board.querySelectorAll('.card')
 
-                 // set default top card position and scale
-                 this.topCard.style.transform =
-                     'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
+      // get top card
+      this.topCard = this.cards[this.cards.length - 1]
 
-                 // destroy previous Hammer instance, if present
-                 if (this.hammer) this.hammer.destroy()
+      // get next card
+      this.nextCard = this.cards[this.cards.length - 2]
 
-                 // listen for tap and pan gestures on top card
-                 this.hammer = new Hammer(this.topCard)
-                 this.hammer.add(new Hammer.Tap())
-                 this.hammer.add(new Hammer.Pan({
-                     position: Hammer.position_ALL,
-                     threshold: 0
-                 }))
+      // if at least one card is present
+      if (this.cards.length > 0) {
 
-                 // pass events data to custom callbacks
-                 this.hammer.on('tap', (e) => {
-                     this.onTap(e)
-                 })
-                 this.hammer.on('pan', (e) => {
-                     this.onPan(e)
-                 })
+         // set default top card position and scale
+         this.topCard.style.transform =
+            'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
 
-             }
+         // destroy previous Hammer instance, if present
+         if (this.hammer) this.hammer.destroy()
 
-         }
+         // listen for tap and pan gestures on top card
+         this.hammer = new Hammer(this.topCard)
+         this.hammer.add(new Hammer.Tap())
+         this.hammer.add(new Hammer.Pan({
+            position: Hammer.position_ALL,
+            threshold: 0
+         }))
 
-         onTap(e) {
+         // pass events data to custom callbacks
+         this.hammer.on('tap', (e) => {
+            this.onTap(e)
+         })
+         this.hammer.on('pan', (e) => {
+            this.onPan(e)
+         })
 
-             // get finger position on top card
-             let propX = (e.center.x - e.target.getBoundingClientRect().left) / e.target.clientWidth
+      }
 
-             // get rotation degrees around Y axis (+/- 15) based on finger position
-             let rotateY = 15 * (propX < 0.05 ? -1 : 1)
+   }
 
-             // enable transform transition
-             this.topCard.style.transition = 'transform 100ms ease-out'
+   onTap(e) {
 
-             // apply rotation around Y axis
-             this.topCard.style.transform =
-                 'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(' + rotateY + 'deg) scale(1)'
+      // get finger position on top card
+      let propX = (e.center.x - e.target.getBoundingClientRect().left) / e.target.clientWidth
 
-             // wait for transition end
-             setTimeout(() => {
-                 // reset transform properties
-                 this.topCard.style.transform =
-                     'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
-             }, 100)
+      // get rotation degrees around Y axis (+/- 15) based on finger position
+      let rotateY = 15 * (propX < 0.05 ? -1 : 1)
 
-         }
+      // enable transform transition
+      this.topCard.style.transition = 'transform 100ms ease-out'
 
-         onPan(e) {
+      // apply rotation around Y axis
+      this.topCard.style.transform =
+         'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(' + rotateY + 'deg) scale(1)'
 
-             if (!this.isPanning) {
+      // wait for transition end
+      setTimeout(() => {
+         // reset transform properties
+         this.topCard.style.transform =
+            'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
+      }, 100)
 
-                 this.isPanning = true
+   }
 
-                 // remove transition properties
-                 this.topCard.style.transition = null
-                 if (this.nextCard) this.nextCard.style.transition = null
+   onPan(e) {
 
-                 // get top card coordinates in pixels
-                 let style = window.getComputedStyle(this.topCard)
-                 let mx = style.transform.match(/^matrix\((.+)\)$/)
-                 this.startPosX = mx ? parseFloat(mx[1].split(', ')[4]) : 0
-                 this.startPosY = mx ? parseFloat(mx[1].split(', ')[5]) : 0
+      if (!this.isPanning) {
 
-                 // get top card bounds
-                 let bounds = this.topCard.getBoundingClientRect()
+         this.isPanning = true
 
-                 // get finger position on top card, top (1) or bottom (-1)
-                 this.isDraggingFrom =
-                     (e.center.y - bounds.top) > this.topCard.clientHeight / 2 ? -1 : 1
+         // remove transition properties
+         this.topCard.style.transition = null
+         if (this.nextCard) this.nextCard.style.transition = null
 
-             }
+         // get top card coordinates in pixels
+         let style = window.getComputedStyle(this.topCard)
+         let mx = style.transform.match(/^matrix\((.+)\)$/)
+         this.startPosX = mx ? parseFloat(mx[1].split(', ')[4]) : 0
+         this.startPosY = mx ? parseFloat(mx[1].split(', ')[5]) : 0
 
-             // get new coordinates
-             let posX = e.deltaX + this.startPosX
-             let posY = e.deltaY + this.startPosY
+         // get top card bounds
+         let bounds = this.topCard.getBoundingClientRect()
 
-             // get ratio between swiped pixels and the axes
-             let propX = e.deltaX / this.board.clientWidth
-             let propY = e.deltaY / this.board.clientHeight
+         // get finger position on top card, top (1) or bottom (-1)
+         this.isDraggingFrom =
+            (e.center.y - bounds.top) > this.topCard.clientHeight / 2 ? -1 : 1
 
-             // get swipe direction, left (-1) or right (1)
-             let dirX = e.deltaX < 0 ? -1 : 1
+      }
 
-             // get degrees of rotation, between 0 and +/- 45
-             let deg = this.isDraggingFrom * dirX * Math.abs(propX) * 45
+      // get new coordinates
+      let posX = e.deltaX + this.startPosX
+      let posY = e.deltaY + this.startPosY
 
-             // get scale ratio, between .95 and 1
-             let scale = (95 + (5 * Math.abs(propX))) / 100
+      // get ratio between swiped pixels and the axes
+      let propX = e.deltaX / this.board.clientWidth
+      let propY = e.deltaY / this.board.clientHeight
 
-             // move and rotate top card
-             this.topCard.style.transform =
-                 'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg) rotateY(0deg) scale(1)'
+      // get swipe direction, left (-1) or right (1)
+      let dirX = e.deltaX < 0 ? -1 : 1
 
-             // scale up next card
-             if (this.nextCard) this.nextCard.style.transform =
-                 'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(' + scale + ')'
+      // get degrees of rotation, between 0 and +/- 45
+      let deg = this.isDraggingFrom * dirX * Math.abs(propX) * 45
 
-             if (e.isFinal) {
+      // get scale ratio, between .95 and 1
+      let scale = (95 + (5 * Math.abs(propX))) / 100
 
-                 this.isPanning = false
+      // move and rotate top card
+      this.topCard.style.transform =
+         'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg) rotateY(0deg) scale(1)'
 
-                 let successful = false
+      // scale up next card
+      if (this.nextCard) this.nextCard.style.transform =
+         'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(' + scale + ')'
 
-                 // set back transition properties
-                 this.topCard.style.transition = 'transform 200ms ease-out'
-                 if (this.nextCard) this.nextCard.style.transition = 'transform 100ms linear'
+      if (e.isFinal) {
 
-                 // check threshold and movement direction
-                 if (propX > 0.25 && e.direction == Hammer.DIRECTION_RIGHT) {
+         this.isPanning = false
 
-                     successful = true
-                     // get right border position
-                     posX = this.board.clientWidth
+         let successful = false
 
-                 } else if (propX < -0.25 && e.direction == Hammer.DIRECTION_LEFT) {
+         // set back transition properties
+         this.topCard.style.transition = 'transform 200ms ease-out'
+         if (this.nextCard) this.nextCard.style.transition = 'transform 100ms linear'
 
-                     successful = true
-                     // get left border position
-                     posX = -(this.board.clientWidth + this.topCard.clientWidth)
+         // check threshold and movement direction
+         if (propX > 0.25 && e.direction == Hammer.DIRECTION_RIGHT) {
 
-                 } else if (propY < -0.25 && e.direction == Hammer.DIRECTION_UP) {
+            successful = true
+            // get right border position
+            posX = this.board.clientWidth
 
-                     successful = true
-                     // get top border position
-                     posY = -(this.board.clientHeight + this.topCard.clientHeight)
+         } else if (propX < -0.25 && e.direction == Hammer.DIRECTION_LEFT) {
 
-                 }
+            successful = true
+            // get left border position
+            posX = -(this.board.clientWidth + this.topCard.clientWidth)
 
-                 if (successful) {
+         } else if (propY < -0.25 && e.direction == Hammer.DIRECTION_UP) {
 
-                     // throw card in the chosen direction
-                     this.topCard.style.transform =
-                         'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg)'
-
-                     // wait transition end
-                     setTimeout(() => {
-                         // remove swiped card
-                         this.board.removeChild(this.topCard)
-                         // add new card
-                         this.push()
-                         // handle gestures on new top card
-                         this.handle()
-                     }, 200)
-
-                 } else {
-
-                     // reset cards position and size
-                     this.topCard.style.transform =
-                         'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
-                     if (this.nextCard) this.nextCard.style.transform =
-                         'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(0.95)'
-
-                 }
-
-             }
+            successful = true
+            // get top border position
+            posY = -(this.board.clientHeight + this.topCard.clientHeight)
 
          }
 
-         push() {
+         if (successful) {
 
-             let card = document.createElement('div')
-             let video = document.createElement("VIDEO")
-             let videosrc = document.createElement('SOURCE')
+            // throw card in the chosen direction
+            this.topCard.style.transform =
+               'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg)'
 
-             card.classList.add('card')
-             video.classList.add('card-video')
+            // wait transition end
+            setTimeout(() => {
+               // remove swiped card
+               this.board.removeChild(this.topCard)
+               // add new card
+               this.push()
+               // handle gestures on new top card
+               this.handle()
+            }, 200)
 
-            video.controls = "controls"
-            video.autoplay = "true"
-            video.preload = "true"
-            videosrc.src = "/assets/video/pompe-expert.mp4"
-            videosrc.type = "video/mp4"
+         } else {
 
-     
-            card.appendChild(video)
-            video.appendChild(videosrc)
-            this.board.insertBefore(card, this.board.firstChild)
-
-
-
+            // reset cards position and size
+            this.topCard.style.transform =
+               'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)'
+            if (this.nextCard) this.nextCard.style.transform =
+               'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(0.95)'
 
          }
 
-     }
+      }
 
-     let board = document.querySelector('#board')
+   }
 
-     let carousel = new Carousel(board)
+   push() {
+      alert(exsData.reps)
+      let card = document.createElement('div')
+      let video = document.createElement("VIDEO")
+      let videosrc = document.createElement('SOURCE')
+
+      card.classList.add('card')
+      video.classList.add('card-video')
+
+      video.controls = "controls"
+      video.autoplay = "true"
+      video.preload = "true"
+      videosrc.src = "/assets/video/pompe-expert.mp4"
+      videosrc.type = "video/mp4"
+
+
+      card.appendChild(video)
+      video.appendChild(videosrc)
+      this.board.insertBefore(card, this.board.firstChild)
+
+
+
+
+   }
+
+}
+
+let board = document.querySelector('#board')
+
+let carousel = new Carousel(board)
+
+
