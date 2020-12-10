e950fd7b0f37cac8e5c6687ee7be6e31afe139a6
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-12-10 13:17:56 +0100

    Adding some javascript, random options picker for the form, select button element

---
+++ assets/js/logic.js
@@ -0,0 +1,32 @@
+const formToObject = (form) =>
+  Array.from(new FormData(form)).reduce(
+    (acc, [key, value]) => ({
+      ...acc,
+      [key]: value,
+    }),
+    {}
+  );
+
+const levelElements = document.querySelectorAll("input[name=level]");
+const workElements = document.querySelectorAll("input[name=work]");
+const confirmButton = document.querySelector("div.config-confirm > button");
+const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+window.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    levelElements[randomInteger(0, 2)].checked = true;
+    workElements[randomInteger(0, 2)].checked = true;
+  },
+  true
+);
+
+
+confirmButton.addEventListener("click", (e) => {
+  startActivityView();
+});
+
+function startActivityView(params) {
+  const userConfig = formToObject(document.querySelector(".config-inputs"));
+  console.log(userConfig);
+}
